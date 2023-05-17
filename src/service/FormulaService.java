db96791c0e74b7806a14e13202858a4b9d777492
ApoTheddy <apotheddy@hotmail.com>
2023-05-17 11:45:36 -0500

    modificando el service

--- src/service/FormulaService.java
+++ src/service/FormulaService.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+
 public class FormulaService {
 
     public Map<String, Double> calcular_sumatoria_simple(int N) {
